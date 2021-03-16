9b9e58f23ac564ca7d790474aa0465d79b1fc3df
Davi Andrade Pontes <daviiap@outlook.com>
2021-03-16 13:48:37 -0300

    Adicionando log para submissao de formulario

--- routes.js
+++ routes.js
@@ -11,7 +11,14 @@ router.get('/', (req, res) => {
 router.post('/submit', (req, res) => {
   const { matricula, nome, contato, description, image } = req.body
 
-  console.log({ matricula, nome, contato, description, image });
+  const date = new Date()
+  console.log('--------------------------------------------------')
+  console.log(`[${date.toLocaleDateString() + ' ' + date.toLocaleTimeString()}]: Formulário submetido!`)
+  console.log(` Nome: ${nome}`)
+  console.log(` Matrícula: ${matricula}`)
+  console.log(` Contato: ${contato}`)
+  console.log('--------------------------------------------------')
+
 
   return res.status(200).json({ matricula, nome, contato, description, image })
 })
