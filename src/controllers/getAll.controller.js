53a7f67bcb402889d99583dc8d16a7d3cb5c5635
Davi Andrade Pontes <daviiap@outlook.com>
2021-03-18 00:01:28 -0300

    Retornando closed no getALl

--- src/controllers/getAll.controller.js
+++ src/controllers/getAll.controller.js
@@ -4,7 +4,7 @@ const getAll = async (req, res) => {
   const formSubmissions = await FormModel
     .find()
     .sort({ created_at: 1 })
-    .select('matricula nome contato description created_at')
+    .select('matricula nome contato description closed created_at')
 
   return res.status(200).json(formSubmissions)
 }
