f386330b310918d19b6826ed87fa87997ba14d89
Davi Andrade Pontes <daviiap@outlook.com>
2021-04-08 23:04:53 -0300

    Mudando a ordenaçao no getAll

--- src/controllers/getAll.controller.js
+++ src/controllers/getAll.controller.js
@@ -6,6 +6,19 @@ const getAll = async (req, res) => {
     .sort({ created_at: 1 })
     .select('matricula nome contato description closed created_at')
 
+  formSubmissions.sort((formItem1, formItem2) => {
+    let compare
+
+    if (formItem1.closed && formItem2.closed) {
+      compare = 0
+    } else if (formItem1.closed && !formItem2.closed) {
+      compare = 1
+    } else {
+      compare = -1
+    }
+
+    return compare
+  })
   return res.status(200).json(formSubmissions)
 }
 
